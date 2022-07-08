ece4978c2586d75d5fac71ae190240860080c034
Robert Espinoza <robert.espinoza@agilebits.com>
2022-07-07 20:26:57 -0700

    update object def

--- tap_netsuite/sync.py
+++ tap_netsuite/sync.py
@@ -117,7 +117,7 @@ def sync_records(ns, catalog_entry, state, counter):
     for page in query_result:
         for rec in page:
             counter.increment()
-            with Transformer(pre_hook=transform_data_hook(ns, stream)) as transformer:
+            with Transformer() as transformer:
                 rec = transformer.transform(serialize_object(rec), schema, catalog_metadata)
 
             singer.write_message(
